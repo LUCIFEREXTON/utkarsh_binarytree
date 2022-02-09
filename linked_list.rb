a8784e510707588437c0e9524899d5eb3e2a5276
LUCIFEREXTON <gupta13utkarsh@gmail.com>
2022-02-09 16:18:12 +0530

    Search and Delete #10

--- linked_list.rb
+++ linked_list.rb
@@ -21,4 +21,38 @@ class LinkedList
       @n += 1
     end
   end
+
+  def search(value, node = @head)
+    until node.nil?
+      return true if node.data == value
+
+      node = node.next
+    end
+    false
+  end
+
+  def remove(value, node = @head)
+    prev = nil
+    while !node.nil? && node.data != value
+      prev = node
+      node = node.next
+    end
+    return if node.nil?
+
+    if !prev.nil?
+      @tail = prev if @tail == node
+      prev.next = node.next
+    else
+      @tail = nil if @tail == @head
+      @head = @head.next
+    end
+  end
+
+  def show(node = @head)
+    until node.nil?
+      print "#{node.data}-->"
+      node = node.next
+    end
+    print "\n"
+  end
 end
--- main9.rb
+++ main9.rb
@@ -4,4 +4,4 @@ ll = LinkedList.new
 
 ll.insert(3)
 ll.insert(4)
-ll.insert(5,6,7,8)
+ll.insert(5, 6, 7, 8)
